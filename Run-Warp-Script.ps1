32c2d9f4484e4b15d8be17e3b1908dc2f5ddaa8a
erik-lance <eriklance@gmail.com>
2023-06-24 20:52:31 +0800

    Added a runner for the script

---
+++ Run-Warp-Script.ps1
@@ -0,0 +1,5 @@
+# This simply runs the `Get-Warp-History.ps1` script and copies the URL to the clipboard.
+# This is to make the powershell window stay open after the script is done.
+
+# Run the script
+powershell -File Get-Warp-History.ps1
