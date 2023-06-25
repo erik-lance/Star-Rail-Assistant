70208ba063cdc299fc830b53af4e8c0c72105a79
erik-lance <eriklance@gmail.com>
2023-06-25 20:50:30 +0800

    Added component content to Import page

---
+++ src/pages/import.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+export default function Import() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Header />
+    
+        <Footer />
+        </main>
+    )
+}
