b14b93568c9595c755b02056965b23b58231f930
erik-lance <eriklance@gmail.com>
2023-06-25 20:30:52 +0800

    Added initial header and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>Footer</h1>
+            <p>Footer content</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+            <p>Header content</p>
+        </div>
+    );
+};
+
+export default Header;
