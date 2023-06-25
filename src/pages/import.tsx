627d6e41f27a4b2b580dd6ca4c11d34cf561f9eb
erik-lance <eriklance@gmail.com>
2023-06-25 21:55:05 +0800

    Added initial import textbox

--- src/pages/import.tsx
+++ src/pages/import.tsx
@@ -4,9 +4,23 @@ import Footer from '@/components/Footer'
 export default function Import() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Header />
-    
-        <Footer />
+            <Header />
+                <div>
+                    <h1>How to import your gacha link?</h1>
+                    <p>Some steps...</p>
+                </div>
+
+                <div
+                    className='rounded-lg p-2 m-2 border-2 border-gray-300 bg-gray-800'
+                >
+                    <h1>Import</h1>
+                    <p>Input your link here</p>
+                    <input type="text" placeholder="https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?" className='
+                    border-2 border-gray-300 rounded-lg p-2 m-2 w-96
+                    '/>
+                </div>
+
+            <Footer />
         </main>
     )
 }
