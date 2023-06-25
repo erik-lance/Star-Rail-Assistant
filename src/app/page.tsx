1b4268be52ba892c9bcbd53b8d1d5ccc5fcc3ece
erik-lance <eriklance@gmail.com>
2023-06-25 21:09:42 +0800

    Formatted indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from 'next/image'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header />
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<Header />
 
-      <Footer />
-    </main>
-  )
+		<Footer />
+		</main>
+	)
 }
