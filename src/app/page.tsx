7a47dae22a8844dc04fdc8103d027aa073cd0fbe
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-06-26 13:05:09 +0800

    Moved header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,10 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+	return <>
 		<Header />
-
-		<Footer />
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 		</main>
-	)
+		<Footer />
+	</>
 }
--- src/pages/import.tsx
+++ src/pages/import.tsx
@@ -2,25 +2,27 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 export default function Import() {
-    return (
+    return <>
+        <Header />
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Header />
-                <div>
-                    <h1>How to import your gacha link?</h1>
-                    <p>Some steps...</p>
-                </div>
 
-                <div
-                    className='rounded-lg p-2 m-2 border-2 border-gray-300 bg-gray-800'
-                >
-                    <h1>Import</h1>
-                    <p>Input your link here</p>
-                    <input type="text" placeholder="https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?" className='
-                    border-2 border-gray-300 rounded-lg p-2 m-2 w-96
-                    '/>
-                </div>
+            <div>
+                <h1>How to import your gacha link?</h1>
+                <p>Some steps...</p>
+            </div>
 
-            <Footer />
+            <div
+                className='rounded-lg p-2 m-2 border-2 border-gray-300 bg-gray-800'
+            >
+                <h1>Import</h1>
+                <p>Input your link here</p>
+                <input type="text" placeholder="https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?" className='
+                        border-2 border-gray-300 rounded-lg p-2 m-2 w-96
+                        '/>
+            </div>
+
+
         </main>
-    )
+        <Footer />
+    </>
 }
