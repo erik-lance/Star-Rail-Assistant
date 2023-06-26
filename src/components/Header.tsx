bc134b06daf9b30ddf063a863ce88387cdda76a4
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-06-26 13:09:09 +0800

    Added navbar links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-            <p>Header content</p>
+        <div className='w-full flex flex-row justify-between items-center p-4'>
+            <a className='text-2xl font-bold' href='/'>Star Rail Assistant</a>
+            <div className='flex flex-row space-x-4'>
+                <a href='import'>Import</a>
+                <a href='export'>Export</a>
+                <a href='about'>About</a>
+                <a href='faq'>FAQ</a>
+                <a href='contact'>Contact</a>
+            </div>
         </div>
     );
 };
