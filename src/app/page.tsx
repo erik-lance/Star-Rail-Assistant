4972f41dd9b76e3b15cc4a9a2a1eec6543e179a5
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-06-26 13:26:04 +0800

    Fixed height spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,20 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 export default function Home() {
-	return <>
+	return <main className='flex flex-col min-h-screen justify-between'>
 		<Header />
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-		</main>
+		<div className="flex flex-col items-center justify-between p-24">
+
+
+
+			<div className="flex flex-col flex-1">
+				<h1 className="text-6xl font-bold">
+					<p>Test</p>
+				</h1>
+			</div>
+
+
+		</div>
 		<Footer />
-	</>
+	</main>
 }
