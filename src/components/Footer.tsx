514821d0aae117c89538c89404356be8f81fc228
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-06-26 13:23:10 +0800

    Added GitHub link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-            <p>Footer content</p>
+        <div className='w-full flex flex-row justify-between items-center p-4'>
+            <div>
+                
+            </div>
+            <div className='flex flex-row space-x-4'>
+                <a href='https://github.com/erik-lance/Star-Rail-Assistant' target='_blank'>
+                    <AiFillGithub size={24} />
+                </a>
+            </div>
         </div>
     );
 };
