f9f21d3ed2612640bb9118a28584d90b8649962a
erik-lance <eriklance@gmail.com>
2023-06-28 01:41:54 +0800

    Added route to history

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ const Header = () => {
         <div className='w-full flex flex-row justify-between items-center p-4'>
             <a className='text-2xl font-bold' href='/'>Star Rail Assistant</a>
             <div className='flex flex-row space-x-4'>
+                <a href='history'>Wish History</a>
                 <a href='import'>Import</a>
                 <a href='export'>Export</a>
                 <a href='about'>About</a>
