09cafdf4457a46d06834e516e160d0cb67927ebc
erik-lance <eriklance@gmail.com>
2023-06-28 23:24:33 +0800

    Now sends gacha data back to import

--- src/pages/api/import-gacha-endpoint.ts
+++ src/pages/api/import-gacha-endpoint.ts
@@ -1,21 +1,19 @@
-// pages/api/import.ts
-
 import { NextApiRequest, NextApiResponse } from "next";
-import importGacha from "@/api/import-gacha";
+import importGacha, { GachaItem } from "@/api/import-gacha";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     console.log("Starting gacha import");
     if (req.method === "POST") {
         try {
             const { link } = req.body;
-            await importGacha(link);
+            const gachaData: GachaItem[] = await importGacha(link);
 
-            res.status(200).json({ message: "Gacha import completed successfully" });
+            res.status(200).json({ success: true, gachaData });
         } catch (error) {
             console.error("Error importing gacha data", error);
-            res.status(500).json({ error: "Error importing gacha data" });
+            res.status(500).json({ success: false, error: "Error importing gacha data" });
         }
     } else {
-        res.status(405).json({ error: "Method Not Allowed" });
+        res.status(405).json({ success: false, error: "Method Not Allowed" });
     }
 }
