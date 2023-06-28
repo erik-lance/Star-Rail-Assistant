9859765b00c41dd2b747bb4104f14aecba3fb7d7
erik-lance <eriklance@gmail.com>
2023-06-28 21:57:07 +0800

    Added handler to call API endpoint to fetch data

--- src/pages/import.tsx
+++ src/pages/import.tsx
@@ -1,52 +1,73 @@
-import CodeBlock from '@/components/CodeBlock'
-import importGacha from '@/api/import-gacha'
-
+import CodeBlock from "@/components/CodeBlock";
+import { useState } from "react";
 
 export default function Import() {
-    return <>
-        <div
-            className='rounded-lg p-4 m-2 border-2 border-gray-300 bg-gray-800 mb-10 w-1/2'
-        >
-            <h1 className='text-2xl font-bold'>How to import your gacha link?</h1>
-            <p>Follow these steps!</p>
-            <ol className='list-decimal list-inside'>
-                <li>Open Windows PowerShell</li>
-                <li>Copy and paste the command below</li>
-                <CodeBlock
-                    text={`
-                        $scriptUrl = 'https://raw.githubusercontent.com/erik-lance/Star-Rail-Assistant/master/powershell_scripts/Get-Warp-History.ps1'
-                        $scriptPath = Join-Path -Path $env:TEMP -ChildPath 'Get-Warp-History.ps1'
-                        Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath
-                        powershell -NoExit -File $scriptPath
-                        Remove-Item -Path $scriptPath -Force
+    const [link, setLink] = useState("");
+
+    const handleImport = async () => {
+        try {
+            console.log("Handling Import")
+            const response = await fetch("/api/import", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ link }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                // Handle the successful response from the API
+            } else {
+                console.error("Error importing gacha data");
+                // Handle the error response from the API
+            }
+        } catch (error) {
+            console.error("Error importing gacha data", error);
+            // Handle the fetch error
+        }
+    };
+
+    return (
+        <>
+            <div className="rounded-lg p-4 m-2 border-2 border-gray-300 bg-gray-800 mb-10 w-1/2">
+                <h1 className="text-2xl font-bold">How to import your gacha link?</h1>
+                <p>Follow these steps!</p>
+                <ol className="list-decimal list-inside">
+                    <li>Open Windows PowerShell</li>
+                    <li>Copy and paste the command below</li>
+                    <CodeBlock
+                        text={`
+                            $scriptUrl = 'https://raw.githubusercontent.com/erik-lance/Star-Rail-Assistant/master/powershell_scripts/Get-Warp-History.ps1'
+                            $scriptPath = Join-Path -Path $env:TEMP -ChildPath 'Get-Warp-History.ps1'
+                            Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath
+                            powershell -NoExit -File $scriptPath
+                            Remove-Item -Path $scriptPath -Force
                     `}
+                    />
+                    <li>This grabs your temporary authkey. Now paste that into the textbox of this page below.</li>
+                </ol>
+            </div>
+
+            <div className="rounded-lg p-2 m-2 border-2 border-gray-300 bg-gray-800">
+                <h1 className="text-2xl font-bold">Import</h1>
+                <p>Input your link here</p>
+                <input
+                    type="text"
+                    placeholder="https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?"
+                    className="border-2 border-gray-300 rounded-lg p-2 m-2 w-96"
+                    id="import-link"
+                    value={link}
+                    onChange={(e) => setLink(e.target.value)}
                 />
-                <li>This grabs your temporary authkey. Now paste that into the textbox of this page below.</li>
-
-
-            </ol>
-        </div>
-
-        <div
-            className='rounded-lg p-2 m-2 border-2 border-gray-300 bg-gray-800'
-        >
-            <h1 className='text-2xl font-bold'>Import</h1>
-            <p>Input your link here</p>
-            <input type="text" placeholder="https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?" className='
-                        border-2 border-gray-300 rounded-lg p-2 m-2 w-96
-                        '
-                        id='import-link'
-                        />
-            <button className='
-                        border-2 border-gray-300 rounded-lg p-2 m-2
-                        '
-                        onClick={() => {
-                            // Import gacha with the link
-                            const link = (document.getElementById('import-link') as HTMLInputElement).value
-                            importGacha(link)
-                        }}
-                        >Import</button>
-        </div>
-
-    </>
+                <button
+                    className="border-2 border-gray-300 rounded-lg p-2 m-2"
+                    onClick={handleImport}
+                >
+                    Import
+                </button>
+            </div>
+        </>
+    );
 }
