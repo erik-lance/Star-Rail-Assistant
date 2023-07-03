4838b25ba4efaa427c2b2d7377ffff8772310d4b
erik-lance <eriklance@gmail.com>
2023-07-03 23:04:15 +0800

    Added initial fetch player data

--- src/api/player-details.ts
+++ src/api/player-details.ts
@@ -1,5 +1,3 @@
-// URL ENDPOINT: https://api.mihomo.me/sr_info_parsed/${uuid}?lang=en
-
 /**
  * This function returns the link to the player's data
  * @param uuid Player's uuid to fetch the data from
@@ -7,4 +5,35 @@
  */
 function player_api_link(uuid: string): string {
     return `https://api.mihomo.me/sr_info_parsed/${uuid}?lang=en`;
+}
+
+export default async function importPlayer(uuid: string) {
+    console.log("Starting player import script")
+    const player_url = player_api_link(uuid);
+    const response = await fetch(player_url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    // Note: the last 3 keys are found inside space_info
+    const player_data = {
+        nickname: "",
+        level: 0,
+        world_level: 0,
+        friend_count: 0,
+        avatar: {
+            name: "",
+        },
+        signature: "",
+
+        light_cone_count: 0,
+        avatar_count: 0,
+        achievement_count: 0
+    }
+
+    console.log("Player import completed successfully");
+
+    // Store the data
 }
